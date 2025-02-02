d8dbcbe026fe64b2f07dd9019972c985775a3324
Devansh Rathi <142798393+devanshrathi11@users.noreply.github.com>
2025-02-02 20:24:06 +0530

    Add files via upload

---
+++ rotated_arr.java
@@ -0,0 +1,12 @@
+public class rotated_arr {
+    public boolean check(int[] nums) {
+        int cnt = 0;
+        for (int i = 0, n = nums.length; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                ++cnt;
+            }
+        }
+        return cnt <= 1;
+    }
+    
+}
