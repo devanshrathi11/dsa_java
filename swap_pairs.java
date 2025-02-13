5c8c9001dfaf8e97b5329a402450c3c5eaba79c0
Devansh Rathi <142798393+devanshrathi11@users.noreply.github.com>
2025-02-13 21:17:49 +0530

    Add files via upload

---
+++ swap_pairs.java
@@ -0,0 +1,24 @@
+public class swap_pairs {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            ListNode t = cur.next;
+            cur.next = t.next;
+            t.next = cur;
+            pre.next = t;
+            pre = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
